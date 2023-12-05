200e096b74e5f2b065f4f6feacafee90e9f1931e
Jordan-Walters-23 <128825567+Jordan-Walters-23@users.noreply.github.com>
2023-12-05 14:26:35 +0000

    center styling

--- app/WelcomePage/page.js
+++ app/WelcomePage/page.js
@@ -1,29 +1,55 @@
-'use client';
+"use client";
 
-import { Link } from '@chakra-ui/next-js';
-import { Box } from '@chakra-ui/react';
-import { Text } from '@chakra-ui/react';
-import { Center, Square, Circle } from '@chakra-ui/react';
-import { Image } from '@chakra-ui/react';
+import { Link } from "@chakra-ui/next-js";
+import { Box, VStack } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import { Center, Square, Circle } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 
 export default function WelcomePage() {
   return (
     <>
-      <h1>Welcome to the welcome page</h1>
-      <Box as='section' bg={'#D9D9D6'} borderRadius={'lg'} w={'75%'} m={'auto'} p={'auto'}>
-        <Center>Welcome back USERNAME!</Center>
-      </Box>
       <Center>
-        <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov'></Image>
+        <VStack spacing={"24px"}>
+          <Box
+            as="section"
+            bg={"#D9D9D6"}
+            borderRadius={"lg"}
+            w={"75%"}
+            marginTop={"5rem"}
+          >
+            <Center>Welcome back USERNAME!</Center>
+          </Box>
+          <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov"></Image>
+          <Box
+            as="section"
+            bg={"#D9D9D6"}
+            borderRadius={"lg"}
+            w={"75%"}
+            m={"auto"}
+            p={"0.5rem"}
+          >
+            <Text>
+              READING NOTES: Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat.
+            </Text>
+          </Box>
+          <Box
+            as="section"
+            bg={"#D9D9D6"}
+            borderRadius={"lg"}
+            w={"75%"}
+            m={"auto"}
+            p={"auto"}
+          >
+            <Center>GO BACK TO READ!</Center>
+          </Box>
+        </VStack>
       </Center>
-      <Box as='section' bg={'#D9D9D6'} borderRadius={'lg'} w={'75%'} m={'auto'} p={'auto'}>
-        <Center>READING NOTES</Center>
-      </Box>
-      <Box as='section' bg={'#D9D9D6'} borderRadius={'lg'} w={'75%'} m={'auto'} p={'auto'}>
-        <Center>GO BACK TO READ!</Center>
-      </Box>
       <button>
-        <Link className='returnButton' href='/'>
+        <Link className="returnButton" href="/">
           Return Home
         </Link>
       </button>
