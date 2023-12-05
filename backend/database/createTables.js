0f3f5bc1b05b8c2bbda6f1a425b784bd68bed810
jacobfield <jacobprogrammes@gmail.com>
2023-12-05 13:51:20 +0000

    installing dotenv and fixing .env variables

--- backend/database/createTables.js
+++ backend/database/createTables.js
@@ -6,27 +6,32 @@ export async function createTables() {
     await pool.query(`
         CREATE TABLE users(
             id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-            nickname VARCHAR(100) NOT NULL,
+            nickname VARCHAR(100) NOT NULL
         );`);
+
     // creating reading_notes table
-    await pool.query(`CREATE TABLE reading_notes(
-                id INT GENERATED ALWAYS,
-                date DATE DEFAULT CURRENT_DATE,
-                title VARCHAR(225) NOT NULL,
-                author VARCHAR(225) NOT NULL,   
-                notes TEXT NOT NULL,
-                users_id INT REFERENCES users(id) ON DELETE CASCADE,
-            );`);
+    await pool.query(`
+        CREATE TABLE reading_notes(
+            id INT GENERATED ALWAYS,
+            date DATE DEFAULT CURRENT_DATE,
+            title VARCHAR(225) NOT NULL,
+            author VARCHAR(225) NOT NULL,   
+            notes TEXT NOT NULL,
+            users_id INT REFERENCES users(id) ON DELETE CASCADE
+        );`);
+
     // creating avatars table
-    await pool.query(`CREATE TABLE avatars(
-                id INT GENERATED ALWAYS,
-                avatar_img VARCHAR(500) NOT NULL,
-                users_id INT REFERENCES users(id) ON DELETE CASCADE,
-            )`);
+    await pool.query(`
+        CREATE TABLE avatars(
+            id INT GENERATED ALWAYS,
+            avatar_img VARCHAR(500) NOT NULL,
+            users_id INT REFERENCES users(id) ON DELETE CASCADE
+        );`);
+
     console.log("Tables successfully created. Time to start reading!");
   } catch (error) {
     console.error(
-      "Database reset failed - issue originated in createTables.js: ",
+      "Database reset failed - issue originated in createTables.js",
       error
     );
   }
