28f897477131c93b7a339a8e3dd1f636169b4494
jacobfield <jacobprogrammes@gmail.com>
2023-12-05 12:04:08 +0000

    createTables.js created

---
+++ backend/database/createTables.js
@@ -0,0 +1,33 @@
+import { pool } from "./index.js";
+
+export async function createTables() {
+  try {
+    // creating users table
+    await pool.query(`
+        CREATE TABLE users(
+            id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            nickname VARCHAR(100) NOT NULL,
+        );`);
+    // creating reading_notes table
+    await pool.query(`CREATE TABLE reading_notes(
+                id INT GENERATED ALWAYS,
+                date DATE NOT NULL,
+                title VARCHAR(225) NOT NULL,
+                author VARCHAR(225) NOT NULL,   
+                notes TEXT NOT NULL,
+                users_id INT REFERENCES users(id) ON DELETE CASCADE,
+            );`);
+    // creating avatars table
+    await pool.query(`CREATE TABLE avatars(
+                id INT GENERATED ALWAYS,
+                avatar_img VARCHAR(500) NOT NULL,
+                users_id INT REFERENCES users(id) ON DELETE CASCADE,
+            )`);
+    console.log("Tables successfully created. Time to start reading!");
+  } catch (error) {
+    console.error(
+      "Database reset failed - issue originated in createTables.js: ",
+      error
+    );
+  }
+}
