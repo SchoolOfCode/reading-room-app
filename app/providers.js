574ebdbf5a8e030dba8afee6124568f918ba1e94
mickeymarse <mmserrapica@tutanota.com>
2023-12-05 16:31:09 +0000

    Tried an failed to install font

--- app/providers.js
+++ app/providers.js
@@ -1,12 +1,14 @@
 'use client';
 
+import '@fontsource/bubblegum-sans';
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme.js';
 
 export function Providers({ children }) {
   return (
     <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
     </CacheProvider>
   );
 }
---
+++ app/theme.js
@@ -0,0 +1,9 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    body: `'Bubblegum Sans', sans-serif`,
+  },
+});
+
+export default theme;
