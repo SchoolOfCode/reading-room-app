f21e8cea586b7f509d35e561963d285f0b72f0c0
mickeymarse <mmserrapica@tutanota.com>
2023-12-05 15:03:59 +0000

    Added book icon in welcome page and resized placeholder avatar

--- app/WelcomePage/page.js
+++ app/WelcomePage/page.js
@@ -1,60 +1,45 @@
-"use client";
+'use client';
 
-import { Link } from "@chakra-ui/next-js";
-import { Box, VStack, flexbox } from "@chakra-ui/react";
-import { Text } from "@chakra-ui/react";
-import { Center, Square, Circle } from "@chakra-ui/react";
-import { Image } from "@chakra-ui/react";
+import { Link } from '@chakra-ui/next-js';
+import { Box, VStack, Text, Center, Image, Icon } from '@chakra-ui/react';
+import { FaBookOpen } from 'react-icons/fa';
 
 export default function WelcomePage() {
   return (
     <>
       <Center>
-        <VStack spacing={"24px"}>
-          <Box
-            as="section"
-            bg={"#D9D9D6"}
-            borderRadius={"lg"}
-            w={"75%"}
-            marginTop={"5rem"}
-          >
+        <VStack spacing={'24px'}>
+          <Box as='section' bg={'#D9D9D6'} borderRadius={'lg'} w={'75%'} marginTop={'5rem'}>
             <Center>Welcome back USERNAME!</Center>
           </Box>
-          <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov"></Image>
-          <Box
-            as="section"
-            bg={"#D9D9D6"}
-            borderRadius={"lg"}
-            w={"75%"}
-            m={"auto"}
-            p={"0.5rem"}
-          >
+          <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' boxSize={'150'}></Image>
+          <Box as='section' bg={'#D9D9D6'} borderRadius={'lg'} w={'75%'} m={'auto'} p={'0.5rem'}>
             <Text>
-              READING NOTES: Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris nisi ut aliquip ex ea commodo consequat.
+              READING NOTES: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </Text>
           </Box>
           <Box
-            display={"flexbox"}
-            textAlign={"center"}
-            as="section"
-            bg={"#D9D9D6"}
-            borderRadius={"lg"}
-            w={"75%"}
-            m={"auto"}
-            p={"auto"}
+            display={'flexbox'}
+            textAlign={'center'}
+            as='section'
+            bg={'#D9D9D6'}
+            borderRadius={'lg'}
+            w={'75%'}
+            m={'auto'}
+            p={'auto'}
           >
+            <Text>Go to the Reading Room</Text>
             <Center>
-              Go to the Reading Room <br></br>Book<br></br> and continue
-              reading!{" "}
+              <Icon as={FaBookOpen} boxSize={'10'}></Icon>
             </Center>
+            <Text>and continue reading!</Text>
           </Box>
         </VStack>
       </Center>
       <button>
-        <Link className="returnButton" href="/">
+        <Link className='returnButton' href='/'>
           Return Home
         </Link>
       </button>
