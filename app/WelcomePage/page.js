260712ca9c45fb381abcfec5702dd2c16bfac54c
mickeymarse <mmserrapica@tutanota.com>
2023-12-05 12:29:56 +0000

    Added placeholder boxes for text and a mock image to test component

--- app/WelcomePage/page.js
+++ app/WelcomePage/page.js
@@ -1,11 +1,29 @@
-import Link from "next/link";
+'use client';
+
+import { Link } from '@chakra-ui/next-js';
+import { Box } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
+import { Center, Square, Circle } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
 
 export default function WelcomePage() {
   return (
     <>
       <h1>Welcome to the welcome page</h1>
+      <Box as='section' bg={'#D9D9D6'} borderRadius={'lg'} w={'75%'} m={'auto'} p={'auto'}>
+        <Center>Welcome back USERNAME!</Center>
+      </Box>
+      <Center>
+        <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov'></Image>
+      </Center>
+      <Box as='section' bg={'#D9D9D6'} borderRadius={'lg'} w={'75%'} m={'auto'} p={'auto'}>
+        <Center>READING NOTES</Center>
+      </Box>
+      <Box as='section' bg={'#D9D9D6'} borderRadius={'lg'} w={'75%'} m={'auto'} p={'auto'}>
+        <Center>GO BACK TO READ!</Center>
+      </Box>
       <button>
-        <Link className="returnButton" href="/">
+        <Link className='returnButton' href='/'>
           Return Home
         </Link>
       </button>
--- app/page.js
+++ app/page.js
@@ -1,16 +1,21 @@
-import Link from "next/link";
+'use client';
+
+import { Link } from '@chakra-ui/next-js';
+import { Box } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <>
       <h1>The Reading Room Home Page</h1>
+
       <button>
-        <Link className="welcomePageLink" href="/WelcomePage">
+        <Link color='blue.400' _hover={{ color: 'blue.500' }} href='/WelcomePage'>
           Welcome Page Link
         </Link>
       </button>
       <button>
-        <Link className="readingRoomLink" href="/ReadingRoom">
+        <Link color='blue.800' _hover={{ color: 'blue.200' }} href='/ReadingRoom'>
           Reading Room Link
         </Link>
       </button>
