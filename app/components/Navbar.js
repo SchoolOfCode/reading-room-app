fd6504421741963d36554880ef813530b61dae12
Melissa Baker <mtfourcandles@gmail.com>
2023-12-06 15:00:21 +0000

    Nav bar and Icon

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,3 +1,15 @@
+'use client';
+import { Container } from '@chakra-ui/react'
+import { Icon } from '@chakra-ui/react'
+import { BiSolidHomeHeart } from "react-icons/bi";
+
+
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return(
+   <Container>
+    <Icon as={BiSolidHomeHeart}></Icon>
+    <Icon></Icon>
+    <Icon></Icon>
+  </Container>)
 }
