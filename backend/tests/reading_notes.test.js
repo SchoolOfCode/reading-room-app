6a8a10ddacd1291aa99e84547ef4ff50966d07ed
Jordan-Walters-23 <128825567+Jordan-Walters-23@users.noreply.github.com>
2023-12-06 16:21:22 +0000

    wrote test for "reading_notes" Same structure as user test

--- backend/tests/reading_notes.test.js
+++ backend/tests/reading_notes.test.js
@@ -13,12 +13,34 @@ test("Get reading_notes test", async function () {
   const response = await request(app).get("/reading_notes");
 
   // assert status code respons
-  expext(response.status).toBe(200);
+  expect(response.status).toBe(200);
 
   //expect status response body
-  expext(response.body).toStrictEqual();
+  expect(response.body).toStrictEqual({
+    status: "success",
+    data: [
+      {
+        id: 1,
+        date: "2023-12-06T00:00:00.000Z",
+        title: "The Tiger That Came To Tea",
+        author: "Judith Kerr",
+        notes:
+          "The tiger that came to tea is a great book for young readers. The story is about a tiger who comes for tea.",
+        users_id: 1,
+      },
+      {
+        id: 2,
+        date: "2023-12-06T00:00:00.000Z",
+        title: "Where's Wally",
+        author: "Martin Handford",
+        notes:
+          "This is a great book for children. I'm really struggling to find Wally on the third page though!",
+        users_id: 2,
+      },
+    ],
+  });
   //expect response type to be JSON
 
-    // assert body status 
-    expect(response.body.status).toMatch("success");
-  });
+  // assert body status
+  expect(response.body.status).toMatch("success");
+});
