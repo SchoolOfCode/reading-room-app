38cbeb5ca2db0eda9414a9127cf77b34c2bfc738
Melissa Baker <mtfourcandles@gmail.com>
2023-12-06 15:59:29 +0000

    NAVBAR Links and icons- fully connected

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,15 +1,20 @@
 'use client';
 import { Container } from '@chakra-ui/react'
 import { Icon } from '@chakra-ui/react'
+import { FaBookOpen } from 'react-icons/fa';
 import { BiSolidHomeHeart } from "react-icons/bi";
+import { FaSmile } from "react-icons/fa";
+import { Link } from '@chakra-ui/next-js';
+
 
 
 
 export default function Navbar() {
   return(
    <Container>
-    <Icon as={BiSolidHomeHeart}></Icon>
-    <Icon></Icon>
-    <Icon></Icon>
+  <Link href='/WelcomePage'>
+    <Icon as={BiSolidHomeHeart} ></Icon></Link>
+    <Link href='/ReadingRoom'><Icon as={FaBookOpen}></Icon></Link>
+  <Link href='#'><Icon as={FaSmile} ></Icon></Link>
   </Container>)
 }
