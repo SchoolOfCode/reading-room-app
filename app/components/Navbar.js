cef061c6c86d54e2397f0262c6f71bc5fb8bd2aa
mickeymarse <mmserrapica@tutanota.com>
2023-12-07 16:19:04 +0000

    Added colored box under icon to signal current pg

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -15,53 +15,52 @@ export default function Navbar() {
   };
 
   return (
-    <Container bg={'#f3e5ab'} position={'fixed'} bottom={'0'} width={'100%'} zIndex={'base'}>
+    <Container bg={'#F3E5AB'} position={'fixed'} bottom={'0'} width={'100%'}>
       <Flex justify={'space-around'} align={'center'}>
         <Link href='/WelcomePage'>
           <Icon
             as={BiSolidHomeHeart}
-            color={'#00b64c'}
+            color='#00B64C'
             boxSize={'100'}
             onClick={() => handleSetActivePage('WP')}
           />
           <Box
-            as='section'
-            w={'10%'}
-            style={activePage === 'WP' ? { bg: '#00b64c' } : { bg: 'gray' }}
-          >
-            xxx
-          </Box>
+            bg='#00B64C'
+            height='2px'
+            width='100%'
+            mt='2px'
+            display={activePage === 'WP' ? 'block' : 'none'}
+          />
         </Link>
-
         <Link href='/ReadingRoom'>
           <Icon
             as={FaBookOpen}
-            color={'#F34213'}
+            color='#F34213'
             boxSize={'97'}
             onClick={() => handleSetActivePage('RR')}
           />
           <Box
-            as='section'
-            w={'10%'}
-            style={activePage === 'RR' ? { bg: '#F34213' } : { bg: 'gray' }}
-          >
-            xxx
-          </Box>
+            bg='#F34213'
+            height='2px'
+            width='100%'
+            mt='2px'
+            display={activePage === 'RR' ? 'block' : 'none'}
+          />
         </Link>
         <Link href='#'>
           <Icon
             as={FaSmile}
-            color={'#E83F6F'}
+            color='#E83F6F'
             boxSize={'90'}
             onClick={() => handleSetActivePage('A')}
           />
           <Box
-            as='section'
-            w={'10%'}
-            style={activePage === 'A' ? { bg: '#E83F6F' } : { bg: 'gray' }}
-          >
-            xxx
-          </Box>
+            bg='#E83F6F'
+            height='2px'
+            width='100%'
+            mt='2px'
+            display={activePage === 'A' ? 'block' : 'none'}
+          />
         </Link>
       </Flex>
     </Container>
