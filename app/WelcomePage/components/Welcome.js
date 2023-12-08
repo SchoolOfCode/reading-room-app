34978cddbb7388e30805345c1733a193af4392a1
jacobfield <jacobprogrammes@gmail.com>
2023-12-08 16:42:24 +0000

    username populated with Chad

--- app/WelcomePage/components/Welcome.js
+++ app/WelcomePage/components/Welcome.js
@@ -1,20 +1,33 @@
 // Box showing welcoome message to the logged in user
 
-'use client';
+"use client";
 
-import { Box, Center } from '@chakra-ui/react';
+import { Box, Center } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { fetchUserByNickname } from "../../FetchRequests/userByNicknameFetch.js";
 
+//
 export default function Welcome() {
+  // declaring username state
+  const [username, setUsername] = useState("nickname");
+  // using useEffect to make the fetch request
+  useEffect(() => {
+    let sessionUsername = fetchUserByNickname("Chad");
+    setUsername(sessionUsername);
+  }, []);
   return (
     <Box
-      as='section'
-      bg={'#00b64c'}
-      borderRadius={'lg'}
-      w={'75%'}
-      marginTop={'5rem'}
-      boxShadow={'dark-lg'}
+      as="section"
+      bg={"#00b64c"}
+      borderRadius={"lg"}
+      w={"75%"}
+      marginTop={"5rem"}
+      boxShadow={"dark-lg"}
     >
-      <Center as='h3'>Welcome back USERNAME!</Center>
+      <Center as="h3">Welcome back {username}!</Center>
     </Box>
   );
 }
+
+// declare state for username & reading note
+// Use useEffect to call the Fetch request, and update the state with the result of that fetch request
