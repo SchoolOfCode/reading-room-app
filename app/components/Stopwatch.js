cd054f920e46a595ff9411fedfdd9c1ec5b3f8ae
Anna Martins <annamartins@Annas-MacBook-Pro.local>
2023-12-11 11:43:30 +0000

    working on stopwatch to include hours, minutes and seconds

--- app/components/Stopwatch.js
+++ app/components/Stopwatch.js
@@ -14,55 +14,72 @@ const styles = {
 	}, 
 	title: { 
 		fontSize: '25px', 
-		color: '#333', 
+		color: 'whitesmoke', 
 		marginBottom: '10px', 
 		fontFamily: 'Arial, sans-serif', 
 	},  
+	subtitle: {
+		fontSize: '15px', 
+		color: 'whitesmoke', 
+		marginBottom: '10px', 
+		fontFamily: 'Arial, sans-serif', 
+	},
 	time: { 
 		fontSize: '64px', 
-		color: '#333', 
+		color: 'whitesmoke', 
 		fontWeight: 'bold', 
 		marginBottom: '20px', 
+		border: '2px solid whitesmoke', 
+		borderRadius: '5px',
+		padding: '5px', 
 	}, 
 	buttons: { 
 		display: 'flex', 
-		gap: '20px', 
+		gap: '20px',
 	}, 
 	button: { 
 		padding: '10px 20px', 
 		borderRadius: '5px', 
 		fontSize: '16px', 
-		color: 'whitesmoke', 
+		color: '#F34213', 
 		cursor: 'pointer', 
 		border: 'none', 
 		outline: 'none', 
 		boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)', 
 	}, 
 	startButton: { 
-		backgroundColor: '#2ecc71', 
+		backgroundColor: '#F3E5AB', 
 	}, 
 	resetButton: { 
-		backgroundColor: '#e74c3c', 
+		backgroundColor: '#F3E5AB', 
 	}, 
 	resumeButton: { 
-		backgroundColor: '#3498db', 
+		backgroundColor: '#F3E5AB', 
 	}, 
 	pauseButton: { 
-		backgroundColor: '#f39c12', 
+		backgroundColor: '#F3E5AB', 
 	}, 
 }; 
 
 export default function Home() { 
-	const [time, setTime] = useState(0); 
+	const [time, setTime] = useState({ hours: 0, minutes: 0, seconds: 0 });
 	const [running, setRunning] = useState(false); 
 	const intervalRef = useRef(null); 
 	const startTimeRef = useRef(0); 
 
 	const startStopwatch = () => { 
-		startTimeRef.current = Date.now() - time * 100; 
+		startTimeRef.current = Date.now() - (time.hours * 3600 + time.minutes * 60 + time.seconds) * 1000;
 		intervalRef.current = setInterval(() => { 
-			setTime(Math.floor((Date.now() - startTimeRef.current) / 1000)); 
-		}, 1000); 
+			setTime(prevTime => {
+				  const seconds = prevTime.seconds + 1;
+				  const minutes = prevTime.minutes + Math.floor(seconds / 60);
+				  const hours = prevTime.hours + Math.floor(minutes / 60);
+  return {
+    hours: hours,
+    minutes: minutes % 60,
+    seconds: seconds % 60
+  };
+});
 		setRunning(true); 
 	}; 
 
@@ -83,6 +100,18 @@ export default function Home() {
 			setTime(Math.floor((Date.now() - 
 				startTimeRef.current) / 1000)); 
 		}, 1000); 
+		//		startTimeRef.current = Date.now() - (time.hours * 3600 + time.minutes * 60 + time.seconds) * 1000;
+// 		intervalRef.current = setInterval(() => { 
+// 			setTime(prevTime => {
+// 				  const seconds = prevTime.seconds + 1;
+// 				  const minutes = prevTime.minutes + Math.floor(seconds / 60);
+// 				  const hours = prevTime.hours + Math.floor(minutes / 60);
+//   return {
+//     hours: hours,
+//     minutes: minutes % 60,
+//     seconds: seconds % 60
+//   };
+// });
 		setRunning(true); 
 	}; 
 
@@ -96,10 +125,12 @@ export default function Home() {
 	}, [running]); 
 
 	return ( 
+		<Box>
 		<div style={styles.container}> 
 			<h1 style={styles.title}> 
-				Want A Challenge? - Time Your Reading 
+				Stopwatch
 			</h1> 
+			<h2 style={styles.title}>Want A Challenge? - Time Your Reading</h2>
 			<div style={styles.time}> 
 				{time}s 
 			</div> 
@@ -130,9 +161,10 @@ export default function Home() {
 							onClick={resumeStopwatch}> 
 						Resume 
 					</button> 
-				)} 
+				)}
 			</div> 
 		</div> 
+		</Box>
 	); 
 }
 
