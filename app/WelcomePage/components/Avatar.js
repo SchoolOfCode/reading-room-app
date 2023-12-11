86b36a7c21d73da49d986733182dfe7bd500ab12
jacobfield <jacobprogrammes@gmail.com>
2023-12-11 11:51:59 +0000

    spinner added when loading

--- app/WelcomePage/components/Avatar.js
+++ app/WelcomePage/components/Avatar.js
@@ -4,15 +4,15 @@
 import { useState, useEffect } from "react";
 import { Image } from "@chakra-ui/react";
 import { fetchAvatars } from "../../FetchRequests/avatarsFetch.js";
+import { Spinner } from "@chakra-ui/react";
 
 export default function Avatar() {
-  const [avatar, setAvatar] = useState("https://bit.ly/dan-abramov");
+  const [avatar, setAvatar] = useState(null); // Start with null or a default avatar URL
 
   useEffect(() => {
     (async () => {
       try {
         const sessionAvatar = await fetchAvatars();
-        // console.log(`sessionNote = ${sessionNote}`);
         setAvatar(sessionAvatar.avatar);
       } catch (error) {
         console.error("Error fetching avatars:", error);
@@ -20,12 +20,12 @@ export default function Avatar() {
     })();
   }, []); // Empty dependency array to run effect only once on mount
 
+  if (avatar === null) {
+    // Return a spinner or a default loading state while avatar is being fetched
+    return <Spinner />;
+  }
+
   return (
-    <Image
-      src={avatar}
-      alt="Dan Abramov"
-      boxSize={150}
-      boxShadow={"dark-lg"}
-    ></Image>
+    <Image src={avatar} alt="User Avatar" boxSize={150} boxShadow="dark-lg" />
   );
 }
