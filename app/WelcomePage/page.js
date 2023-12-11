7df953c3ce4b0c5513ae9e64c61e6a14da82daad
jacobfield <jacobprogrammes@gmail.com>
2023-12-11 15:13:46 +0000

    planning next steps to render data based on user

--- app/WelcomePage/page.js
+++ app/WelcomePage/page.js
@@ -1,16 +1,16 @@
 // Welcome Page tailored for logged in user
 
-'use client';
+"use client";
 
-import { VStack } from '@chakra-ui/react';
-import Welcome from './components/Welcome.js';
-import Avatar from './components/Avatar.js';
-import ReadingNotes from './components/ReadingNotes.js';
-import Continue from './components/Continue.js';
+import { VStack } from "@chakra-ui/react";
+import Welcome from "./components/Welcome.js";
+import Avatar from "./components/Avatar.js";
+import ReadingNotes from "./components/ReadingNotes.js";
+import Continue from "./components/Continue.js";
 
 export default function WelcomePage() {
   return (
-    <VStack as='main' spacing={12}>
+    <VStack as="main" spacing={12}>
       <Welcome />
       <Avatar />
       <ReadingNotes />
@@ -18,3 +18,9 @@ export default function WelcomePage() {
     </VStack>
   );
 }
+
+// make fetch request here, pass the data down as props to the relevant components
+// render the reactive information in the components
+// have the url take in the input based of a user prompt
+// insert Chakra loading wheel here, and return that if something is null.
+// Reading room will not need any GET gunctionality, only POST, so will work nicely
