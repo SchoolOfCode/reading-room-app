8d5119382e09d2667dcb65684bc309dfea69e1c7
Jordan-Walters-23 <128825567+Jordan-Walters-23@users.noreply.github.com>
2023-12-11 11:31:17 +0000

    Button created Continue component changed into a buttom

--- app/WelcomePage/components/Continue.js
+++ app/WelcomePage/components/Continue.js
@@ -1,28 +1,13 @@
 // Message inviting the user to continue their reading
 
-'use client';
+"use client";
 
-import { Box, Text, Center, Icon } from '@chakra-ui/react';
-import { FaBookOpen } from 'react-icons/fa';
+import { Button } from "@chakra-ui/react";
 
 export default function Continue() {
   return (
-    <Box
-      display={'flexbox'}
-      textAlign={'center'}
-      as='section'
-      bg={'#00b64c'}
-      borderRadius={'lg'}
-      w={'75%'}
-      m={'auto'}
-      p={'auto'}
-      boxShadow={'dark-lg'}
-    >
-      <Text>Go to the Reading Room</Text>
-      <Center>
-        <Icon as={FaBookOpen} boxSize={'10'} alt='Icon of an open book.'></Icon>
-      </Center>
-      <Text>and continue reading!</Text>
-    </Box>
+    <Button colorScheme={"blackAlpha"}>
+      Go to the Reading Room
+    </Button>
   );
 }
