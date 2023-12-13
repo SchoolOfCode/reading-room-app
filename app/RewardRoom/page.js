a5fb8fbe9a5a86a8df61a7f06c0ea52db761ee9f
Anna Martins <annamartins@annas-mbp.home>
2023-12-13 11:32:25 +0000

    linked icon in navbar to reward room

---
+++ app/RewardRoom/page.js
@@ -0,0 +1,29 @@
+
+'use client';
+
+import Link from "next/link";
+import { VStack, Button } from "@chakra-ui/react";
+
+//create a function that takes you to the next page when the icon is clicked.
+export default function RewardRoom() {
+  return (
+    <>
+      {/* <VStack spacing={4}>
+        <h1>Choose a reward and drag it onto your avatar!</h1>
+  
+        <Button colorScheme={"blackAlpha"}>
+          <Link className="returnButton" href="/Avatar">
+            Go to your Avatar
+          </Link>
+        </Button>
+        <button>
+          <Link className="returnButton" href="/">
+            Return Home
+          </Link>
+        </button>
+      </VStack>
+    </> */}
+  );
+}
+
+}
