6618951708549a76896b8b32dbb354d777eae3b9
Jordan-Walters-23 <128825567+Jordan-Walters-23@users.noreply.github.com>
2023-12-13 16:27:15 +0000

    Components reordered return home changed to log out

--- app/ReadingRoom/components/page.js
+++ app/ReadingRoom/components/page.js
@@ -3,19 +3,23 @@
 import BookForm from "./readingRoomSubmitForm";
 import Link from "next/link";
 import StopWatch from "../../components/Stopwatch.js";
-import { VStack, Button, Heading } from "@chakra-ui/react";
+import { VStack, Button, Heading, Container } from "@chakra-ui/react";
 
 export default function ReadingRoom() {
   return (
-    <VStack as="main" spacing={10} bg="#FFD6A5">
-      <Heading mt={50}>Welcome to the Reading Room</Heading>
-      <BookForm />
-      <StopWatch></StopWatch>
-      <Button mb={100}>
-        <Link className="returnButton" href="/">
-          Return Home
-        </Link>
-      </Button>
-    </VStack>
+    <>
+      <Container bg="#FFD6A5" minW="100%" align="right" p={5}>
+        <Button>
+          <Link className="returnButton" href="/">
+            Log out
+          </Link>
+        </Button>
+      </Container>
+      <VStack as="main" spacing={10} bg="#FFD6A5" minHeight="100vh">
+        <Heading mt={10}>Welcome to the Reading Room</Heading>
+        <BookForm />
+        <StopWatch></StopWatch>
+      </VStack>
+    </>
   );
 }
