422b4aa20c7407b852fa491d39f365a6eb0cc7b1
Anna Martins <annamartins@annas-mbp.home>
2023-12-13 16:12:56 +0000

    fixing errors

--- app/Avatar/page.js
+++ app/Avatar/page.js
@@ -1,24 +1,24 @@
 
 'use client';
 
-import { useState, useEffect } from "react";
-import { Container, Flex, Heading, Text, Button } from "@chakra-ui/react";
-import { Image, VStack } from "@chakra-ui/react";
+// import { useState, useEffect } from "react";
+// import { Container, Flex, Heading, Text, Button } from "@chakra-ui/react";
+import {VStack } from "@chakra-ui/react";
 import Avatar from "./components/Avatar.js";
 import Backdrop from "./components/Backdrop.js";
-import AvatarWearingCap from "./components/MiffyCap.js";  
-import AvatarWearingLoveShades from "./components/MiffyLoveHearts.js";
-import AvatarWearingPixelShades from "./components/MiffyPixelShades.js";
-import AvatarWearingScarf from "./components/MiffyScarf.js";
-import AvatarWearingSunhat from "./components/MiffySunhat.js";
-import AvatarWearingTopHat from "./components/MiffyTopHat.js";
-import Shades from "./components/shades.js";
-import PixelShades from "./components/pixelshades.js";
-import LoveHeartShades from "./components/loveheartShades.js";
-import Cap from "./components/cap.js";
-import Scarf from "./components/scarf.js";
-import TopHat from "./components/tophat.js";
-import Sunhat from "./components/sunhat.js";``
+// import AvatarWearingCap from "./components/MiffyCap.js";  
+// import AvatarWearingLoveShades from "./components/MiffyLoveHearts.js";
+// import AvatarWearingPixelShades from "./components/MiffyPixelShades.js";
+// import AvatarWearingScarf from "./components/MiffyScarf.js";
+// import AvatarWearingSunhat from "./components/MiffySunhat.js";
+// import AvatarWearingTopHat from "./components/MiffyTopHat.js";
+// import Shades from "./components/shades.js";
+// import PixelShades from "./components/pixelshades.js";
+// import LoveHeartShades from "./components/loveheartShades.js";
+// import Cap from "./components/cap.js";
+// import Scarf from "./components/scarf.js";
+// import TopHat from "./components/tophat.js";
+// import Sunhat from "./components/sunhat.js";``
 
 
 
