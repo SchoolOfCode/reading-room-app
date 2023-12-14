7d64ac903c9c7b4ee0b2ae20c1c2e57978b720bb
mickeymarse <mmserrapica@tutanota.com>
2023-12-14 10:08:57 +0000

    Removed back button

--- app/page.js
+++ app/page.js
@@ -50,31 +50,10 @@ export default function Home({ searchParams }) {
 
   return (
     <div>
-      <Link href='/'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        >
-          <polyline points='15 18 9 12 15 6' />
-        </svg>{' '}
-        Back
-      </Link>
-
       <form action={signIn}>
-        <label className='text-md' htmlFor='email'>
-          Email
-        </label>
+        <label htmlFor='email'>Email</label>
         <input name='email' placeholder='you@example.com' required />
-        <label className='text-md' htmlFor='password'>
-          Password
-        </label>
+        <label htmlFor='password'>Password</label>
         <input type='password' name='password' placeholder='••••••••' required />
         <button>Sign In</button>
         <button formAction={signUp}>Sign Up</button>
