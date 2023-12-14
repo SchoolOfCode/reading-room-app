8efe28f4a5e35e7a901e832ce0ab2a2cfaf9f28d
mickeymarse <mmserrapica@tutanota.com>
2023-12-14 08:30:49 +0000

    SUPABASE SETUP Completed creation of folders and files. Sign in and sign up is now possible.

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,24 @@
 // Global layout in common with all pages
 
-import { Providers } from "./providers";
-import "./globals.css";
+import { Providers } from './providers';
+import './globals.css';
 // Importing fonts object and passing it down as a prop
-import { fonts } from "./fonts.js";
-import Navbar from "./components/Navbar";
+import { fonts } from './fonts.js';
+import Navbar from './components/Navbar';
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000';
 
 export const metadata = {
-  title: "The Reading Room",
-  description: "Created by The Blurb",
+  metadataBase: new URL(defaultUrl),
+  title: 'The Reading Room',
+  description: 'Created by The Blurb',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${fonts.architects_daughter.className} ${fonts.abel.className}`}
-    >
+    <html lang='en' className={`${fonts.architects_daughter.className} ${fonts.abel.className}`}>
       {/*how to use bith heading and body as attribute  */}
       <body>
         <Providers>{children}</Providers>
--- app/page.js
+++ app/page.js
@@ -18,10 +18,10 @@ export default function Home({ searchParams }) {
     });
 
     if (error) {
-      return redirect('/login?message=Could not authenticate user');
+      return redirect('/?message=Could not authenticate user');
     }
 
-    return redirect('/');
+    return redirect('/WelcomePage');
   };
 
   const signUp = async (formData) => {
@@ -42,18 +42,15 @@ export default function Home({ searchParams }) {
     });
 
     if (error) {
-      return redirect('/login?message=Could not authenticate user');
+      return redirect('/?message=Could not authenticate user');
     }
 
-    return redirect('/login?message=Check email to continue sign in process');
+    return redirect('/?message=Check email to continue sign in process');
   };
 
   return (
-    <div className='flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2'>
-      <Link
-        href='/'
-        className='absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm'
-      >
+    <div>
+      <Link href='/'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           width='24'
@@ -64,48 +61,24 @@ export default function Home({ searchParams }) {
           strokeWidth='2'
           strokeLinecap='round'
           strokeLinejoin='round'
-          className='mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1'
         >
           <polyline points='15 18 9 12 15 6' />
         </svg>{' '}
         Back
       </Link>
 
-      <form
-        className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground'
-        action={signIn}
-      >
+      <form action={signIn}>
         <label className='text-md' htmlFor='email'>
           Email
         </label>
-        <input
-          className='rounded-md px-4 py-2 bg-inherit border mb-6'
-          name='email'
-          placeholder='you@example.com'
-          required
-        />
+        <input name='email' placeholder='you@example.com' required />
         <label className='text-md' htmlFor='password'>
           Password
         </label>
-        <input
-          className='rounded-md px-4 py-2 bg-inherit border mb-6'
-          type='password'
-          name='password'
-          placeholder='••••••••'
-          required
-        />
-        <button className='bg-green-700 rounded-md px-4 py-2 text-foreground mb-2'>Sign In</button>
-        <button
-          formAction={signUp}
-          className='border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2'
-        >
-          Sign Up
-        </button>
-        {searchParams?.message && (
-          <p className='mt-4 p-4 bg-foreground/10 text-foreground text-center'>
-            {searchParams.message}
-          </p>
-        )}
+        <input type='password' name='password' placeholder='••••••••' required />
+        <button>Sign In</button>
+        <button formAction={signUp}>Sign Up</button>
+        {searchParams?.message && <p>{searchParams.message}</p>}
       </form>
     </div>
   );
