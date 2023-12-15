22d237fded9a23388a5e73d112aed3b22967e740
mickeymarse <mmserrapica@tutanota.com>
2023-12-15 11:46:36 +0000

    WELCOME PAGE Added fetched username and started stylig text.

--- app/WelcomePage/components/Continue.js
+++ app/WelcomePage/components/Continue.js
@@ -1,18 +1,19 @@
 // Message inviting the user to continue their reading
 
-"use client";
+'use client';
 
-import { Button, Text } from "@chakra-ui/react";
-import { Link } from "@chakra-ui/next-js";
+import { Button, Text } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/next-js';
 import styles from '../welcomePage.module.css';
 
 export default function Continue() {
   return (
     <>
-    <pageWrapper>
-      <Text mb={"-45px"}>Grab your Favourite book</Text>
-       </pageWrapper>
+      <pageWrapper>
+        <Text mb={'-45px'} as='h3' fontSize='2xl' fontWeight='900'>
+          {`Grab your favourite book\nand let's dive into its world!`}
+        </Text>
+      </pageWrapper>
     </>
-    
   );
 }
