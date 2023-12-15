da259d5787af6a3397c78d82ef3f68762d01d1b8
mickeymarse <mmserrapica@tutanota.com>
2023-12-15 09:49:05 +0000

    HEADING Added the Chakra component and the right font

--- app/Avatar/page.js
+++ app/Avatar/page.js
@@ -1,72 +1,78 @@
-"use client";
+'use client';
 
-import { useState, useEffect } from "react";``
-import {Flex, Spacer, Box, Image } from "@chakra-ui/react";
+import { useState, useEffect } from 'react';
+import { fonts } from '../fonts.js';
+import { Flex, Spacer, Box, Image, Heading } from '@chakra-ui/react';
 // import { fonts } from './fonts.js';
-import Avatar from "./components/Avatar.js";
-import Backdrop from "./components/Backdrop.js";
+import Avatar from './components/Avatar.js';
+import Backdrop from './components/Backdrop.js';
 // import AvatarWearingCap from "./components/MiffyCap.js";
 // import AvatarWearingLoveShades from "./components/MiffyLoveHearts.js";
 // import AvatarWearingPixelShades from "./components/MiffyPixelShades.js";
 // import AvatarWearingScarf from "./components/MiffyScarf.js";
 // import AvatarWearingSunhat from "./components/MiffySunhat.js";
 // import AvatarWearingTopHat from "./components/MiffyTopHat.js";
-import Shades from "./components/Shades.js";
-import PixelShades from "./components/Pixelshades.js";
-import LoveHeartShades from "./components/LoveheartShades.js";
-import Cap from "./components/Cap.js";
+import Shades from './components/Shades.js';
+import PixelShades from './components/Pixelshades.js';
+import LoveHeartShades from './components/LoveheartShades.js';
+import Cap from './components/Cap.js';
 // import Scarf from "./components/scarf.js";
-import TopHat from "./components/Tophat.js";
-import Sunhat from "./components/Sunhat.js";
-``;
+import TopHat from './components/Tophat.js';
+import Sunhat from './components/Sunhat.js';
 
 export default function AvatarPage() {
   return (
     <>
-      <Box as="main" spacing={4} alignItems="center" justifyContent="center">
-        <h1>The Reward Room</h1>
+      <Box as='main' spacing={4} alignItems='center' justifyContent='center'>
+        <Heading as='h1' size='xl' className={fonts.arvo.className}>
+          The Reward Room
+        </Heading>
         <br></br>
-        <h2 >Well done!</h2>
-        <h2>Choose a Reward and see your avatar update!</h2>
+        <Heading as='h2' size='l' className={fonts.arvo.className}>
+          Well done!
+        </Heading>
+        <Heading as='h2' size='l' className={fonts.arvo.className}>
+          Choose a Reward and see your avatar update!
+        </Heading>
         {/* accessory components put inside a flex box to create responsive design with equal spacing. Also user spacer */}
         <Flex>
-          <Box boxsize="sm" >
+          <Box boxsize='sm'>
             <Shades></Shades>
           </Box>
           <Spacer></Spacer>
-          <Box boxsize="sm">
+          <Box boxsize='sm'>
             <PixelShades></PixelShades>
           </Box>
           <Spacer></Spacer>
-          <Box boxsize="sm">
+          <Box boxsize='sm'>
             <LoveHeartShades></LoveHeartShades>
           </Box>
           <Spacer></Spacer>
         </Flex>
         <Flex>
-          <Box boxsize="sm">
+          <Box boxsize='sm'>
             <Cap></Cap>
           </Box>
           <Spacer></Spacer>
-          <Box boxsize="sm">
+          <Box boxsize='sm'>
             <Sunhat></Sunhat>
           </Box>
           <Spacer></Spacer>
-          <Box boxsize="sm">
+          <Box boxsize='sm'>
             <TopHat></TopHat>
           </Box>
           <Spacer></Spacer>
         </Flex>
         <Flex>
-          <Box boxsize="sm">
+          <Box boxsize='sm'>
             <Avatar></Avatar>
           </Box>
         </Flex>
         <Spacer></Spacer>
-        <Box boxsize="sm">
-        <Flex>
-          <Backdrop></Backdrop>
-        </Flex>
+        <Box boxsize='sm'>
+          <Flex>
+            <Backdrop></Backdrop>
+          </Flex>
         </Box>
         {/* N.B. reward items and corresponding avatar wearing the item will need to be added */}
       </Box>
