e8f5b09d8f2f4bf7f866b1dbd1b3fb6376c47daf
Melissa Baker <mtfourcandles@gmail.com>
2023-12-18 17:51:22 +0000

    AVATAR PUSH: background rendering but not avatar or accessories.

--- app/Avatar/page.js
+++ app/Avatar/page.js
@@ -1,37 +1,69 @@
+"use client";
 
-'use client';
+import React, { useState } from 'react';
+import Avatar from './components/Avatar';
+import Backdrop from './components/Backdrop';
+// Import accessory images
+import AvatarWearingShades from './components/MiffyShades';
+import AvatarWearingLoveShades from './components/MiffyLoveHearts';
+import AvatarWearingPixelShades from './components/MiffyPixelShades';
+import AvatarWearingCap from './components/MiffyCap';
+import AvatarWearingScarf from './components/MiffyScarf';
+import AvatarWearingTopHat from './components/MiffyTopHat';
+import AvatarWearingSunhat from './components/MiffySunhat';
 
-// import { useState, useEffect } from "react";
-// import { Container, Flex, Heading, Text, Button } from "@chakra-ui/react";
-import {VStack } from "@chakra-ui/react";
-import Avatar from "./components/Avatar.js";
-import Backdrop from "./components/Backdrop.js";
-// import AvatarWearingCap from "./components/MiffyCap.js";  
-// import AvatarWearingLoveShades from "./components/MiffyLoveHearts.js";
-// import AvatarWearingPixelShades from "./components/MiffyPixelShades.js";
-// import AvatarWearingScarf from "./components/MiffyScarf.js";
-// import AvatarWearingSunhat from "./components/MiffySunhat.js";
-// import AvatarWearingTopHat from "./components/MiffyTopHat.js";
-// import Shades from "./components/shades.js";
-// import PixelShades from "./components/pixelshades.js";
-// import LoveHeartShades from "./components/loveheartShades.js";
-// import Cap from "./components/cap.js";
-// import Scarf from "./components/scarf.js";
-// import TopHat from "./components/tophat.js";
-// import Sunhat from "./components/sunhat.js";``
+const CharacterPage = () => {
+  const [selectedAccessory, setSelectedAccessory] = useState(null);
+  const [characterImage, setCharacterImage] = useState('defaultImage.png'); // Placeholder default image
 
+  const handleAccessoryClick = (accessory) => {
+    setSelectedAccessory(accessory);
+    setCharacterImage(accessoryImages[accessory]); // Update character image based on selected accessory
+  };
 
+  const accessories = ['shades', 'loveShades', 'pixelShades', 'cap', 'scarf', 'topHat', 'sunhat'];
 
-export default function AvatarPage () {
+  // Define accessory images object
+  const accessoryImages = {
+    shades: AvatarWearingShades,
+    loveShades: AvatarWearingLoveShades,
+    pixelShades: AvatarWearingPixelShades,
+    cap: AvatarWearingCap,
+    scarf: AvatarWearingScarf,
+    topHat: AvatarWearingTopHat,
+    sunhat: AvatarWearingSunhat,
+  };
+
   return (
-    <>
-    <VStack as="main" spacing={4} bg='#FDFFAB'>
-    <h1>Well done!</h1>
-    <h2>Choose a Reward and see your avatar update!</h2>
-    <Avatar></Avatar>
-    <Backdrop></Backdrop>
-{/* N.B. reward items and corresponding avatar wearing the item will need to be added */}
-    </VStack>
-    </>
-  )
-}
+    <div>
+      {/* Render the backdrop with the background image */}
+      <Backdrop backgroundImage="Backdrop.png" />
+
+      {/* Ensure selectedAccessory is defined before rendering Avatar */}
+      {selectedAccessory !== null && (
+        <div style={{ position: 'relative' }}>
+       <img src="Avatar.png" alt="Avatar" style={{ width: '100px', height: '100px' }} />
+          <Avatar accessory={selectedAccessory} />
+        </div>
+      )}
+
+      {/* Render selected character image */}
+      <img src={characterImage} alt="Avatar" style={{ width: '200px', height: '300px' }} />
+
+      {/* Render accessories */}
+      <div>
+        {accessories.map((accessory, index) => (
+          <img
+            key={index}
+            src={accessoryImages[accessory]} // Render the accessory images
+            alt={accessory}
+            style={{ width: '50px', height: '50px', cursor: 'pointer' }}
+            onClick={() => handleAccessoryClick(accessory)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CharacterPage;
