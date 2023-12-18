925f0b657d51f6812544741bcf708adcd222cd13
Melissa Baker <mtfourcandles@gmail.com>
2023-12-18 21:53:57 +0000

    AVATAR: now wears accessories

--- app/Avatar/page.js
+++ app/Avatar/page.js
@@ -4,6 +4,14 @@ import React, { useState } from 'react';
 import Avatar from './components/Avatar';
 import Backdrop from './components/Backdrop';
 // Import accessory images
+import Shades from './components/Shades';
+import PixelShades from './components/Pixelshades';
+import LoveShades from './components/LoveHeartShades';
+import Cap from './components/Cap';
+import Scarf from './components/Scarf';
+import Sunhat from './components/Sunhat';
+import TopHat from './components/Tophat';
+//Avatar wearing accessories
 import AvatarWearingShades from './components/MiffyShades';
 import AvatarWearingLoveShades from './components/MiffyLoveHearts';
 import AvatarWearingPixelShades from './components/MiffyPixelShades';
@@ -12,55 +20,85 @@ import AvatarWearingScarf from './components/MiffyScarf';
 import AvatarWearingTopHat from './components/MiffyTopHat';
 import AvatarWearingSunhat from './components/MiffySunhat';
 
+const accessoryImages = {
+  shades: 'shades.png',
+  loveShades: 'loveheartshades.png',
+  pixelShades: 'pixel_shades.png',
+  cap: 'cap.png',
+  tophat: 'tophat.png',
+  sunhat: 'sunhat.png',
+  scarf: 'scarf_orange.png',
+};
+
+const avatarImagesWithAccessories = {
+  shades: 'Miffy_shades.png',
+  loveShades: 'Miffy_lovehearts.png',
+  pixelShades: 'Miffy_pixel_shades.png',
+  cap: 'Miffy_cap.png',
+  scarf: 'Miffy_scarf_orange.png',
+  topHat: 'Miffy_tophat.png',
+  sunhat: 'Miffy_Sunhat.png',
+};
+
 const CharacterPage = () => {
   const [selectedAccessory, setSelectedAccessory] = useState(null);
-  const [characterImage, setCharacterImage] = useState('defaultImage.png'); // Placeholder default image
+  const [characterImage, setCharacterImage] = useState('Miffy.png');
 
   const handleAccessoryClick = (accessory) => {
     setSelectedAccessory(accessory);
-    setCharacterImage(accessoryImages[accessory]); // Update character image based on selected accessory
+    setCharacterImage(avatarImagesWithAccessories[accessory] || 'Miffy.png'); // Update character image based on selected accessory
   };
 
-  const accessories = ['shades', 'loveShades', 'pixelShades', 'cap', 'scarf', 'topHat', 'sunhat'];
-
-  // Define accessory images object
-  const accessoryImages = {
-    shades: AvatarWearingShades,
-    loveShades: AvatarWearingLoveShades,
-    pixelShades: AvatarWearingPixelShades,
-    cap: AvatarWearingCap,
-    scarf: AvatarWearingScarf,
-    topHat: AvatarWearingTopHat,
-    sunhat: AvatarWearingSunhat,
-  };
+  const accessories = ['shades', 'loveShades', 'pixelShades', 'cap', 'scarf', 'tophat', 'sunhat'];
 
   return (
     <div>
       {/* Render the backdrop with the background image */}
       <Backdrop backgroundImage="Backdrop.png" />
 
-      {/* Ensure selectedAccessory is defined before rendering Avatar */}
-      {selectedAccessory !== null && (
-        <div style={{ position: 'relative' }}>
-       <img src="Avatar.png" alt="Avatar" style={{ width: '100px', height: '100px' }} />
-          <Avatar accessory={selectedAccessory} />
-        </div>
-      )}
-
       {/* Render selected character image */}
       <img src={characterImage} alt="Avatar" style={{ width: '200px', height: '300px' }} />
 
-      {/* Render accessories */}
+      {/* Display accessories */}
       <div>
-        {accessories.map((accessory, index) => (
-          <img
-            key={index}
-            src={accessoryImages[accessory]} // Render the accessory images
-            alt={accessory}
-            style={{ width: '50px', height: '50px', cursor: 'pointer' }}
-            onClick={() => handleAccessoryClick(accessory)}
-          />
-        ))}
+        {/* Render accessories above the avatar */}
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {accessories.slice(0, 4).map((accessory, index) => (
+            <img
+              key={index}
+              src={accessoryImages[accessory]} // Render the accessory images
+              alt={accessory}
+              style={{ width: '50px', height: '50px', cursor: 'pointer', margin: '5px' }}
+              onClick={() => handleAccessoryClick(accessory)}
+            />
+          ))}
+        </div>
+
+        {/* Render avatar with selected accessory */}
+        <div style={{ position: 'relative', textAlign: 'center' }}>
+          {selectedAccessory !== null && (
+            <div style={{ position: 'relative' }}>
+              <img
+                src={avatarImagesWithAccessories[selectedAccessory] || 'Avatar.png'}
+                alt="Avatar"
+                style={{ width: '100px', height: '100px' }}
+              />
+            </div>
+          )}
+        </div>
+
+        {/* Render accessories below the avatar */}
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {accessories.slice(4).map((accessory, index) => (
+            <img
+              key={index}
+              src={accessoryImages[accessory]} // Render the accessory images
+              alt={accessory}
+              style={{ width: '50px', height: '50px', cursor: 'pointer', margin: '5px' }}
+              onClick={() => handleAccessoryClick(accessory)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
