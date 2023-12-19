9a35a6167ee9bf41d867eb0beecb8d90a4002901
mickeymarse <mmserrapica@tutanota.com>
2023-12-19 12:05:55 +0000

    NAVBAR Added responsiveness. Working on fixing the box below the icons.

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { Container, Flex, Icon, Box } from '@chakra-ui/react';
 import { Link } from '@chakra-ui/next-js';
@@ -11,9 +11,8 @@ import { GiPawPrint } from 'react-icons/gi';
 import { GiSpellBook } from 'react-icons/gi';
 import { useState } from 'react';
 
-
 export default function Navbar() {
-  const [activePage, setActivePage] = useState("");
+  const [activePage, setActivePage] = useState('');
 
   // Function to update the activePage state
   const handleSetActivePage = (page) => {
@@ -21,51 +20,56 @@ export default function Navbar() {
   };
 
   return (
-    <Container bg="#DDD9D1" position="fixed" bottom="0" width="100%" height="75px" pt="5px">
-      <Flex justify="space-around" align="center">
-        <Link href="/WelcomePage" height="75px">
+    <Container bg='#DDD9D1' position='fixed' bottom='0' minWidth='100%' minHeight='10%' pt='5px'>
+      <Flex justify='space-around' align='center'>
+        <Link href='/WelcomePage' height='75px'>
           <Icon
             as={HiBuildingStorefront}
-            color="#747474"
-            boxSize={50} // Adjust icons height here
+            color='#747474'
+            height='90%'
+            width='90%'
             onClick={() => handleSetActivePage('WP')}
           />
           <Box
-            bg="#CAFFBF"
-            border="1px solid #747474" // Added border due to light colours blending to the background
-            height="5px"
+            bg='#CAFFBF'
+            border='1px solid #747474' // Added border due to light colours blending to the background
+            height='7px'
+            mr='2px'
             display={activePage === 'WP' ? 'block' : 'none'}
           />
         </Link>
-        <Link href="/ReadingRoom" height="75px">
+        <Link href='/ReadingRoom' height='75px'>
           <Icon
             as={GiSpellBook}
-            color="#747474"
-            width="50px"
-            boxSize={50} // Adjust icons height here
+            color='#747474'
+            height='90%'
+            width='90%'
             onClick={() => handleSetActivePage('RR')}
           />
           <Box
-            bg="#FFD6A5"
-            border="1px solid #747474"
-            height="5px"
-            mt="2px"
+            bg='#FFD6A5'
+            border='1px solid #747474'
+            height='7px'
+            mr='2px'
+            mt='2px'
             display={activePage === 'RR' ? 'block' : 'none'}
           />
         </Link>
 
-        <Link href='/Avatar' height="75px">
+        <Link href='/Avatar' height='75px'>
           <Icon
             as={GiPawPrint}
-            color="#747474"
-            boxSize={50} // Adjust icons height here
+            color='#747474'
+            height='90%'
+            width='90%'
             onClick={() => handleSetActivePage('A')}
           />
           <Box
-            bg="#FDFFAB"
-            border="1px solid #747474"
-            height="5px"
-            mt="2px"
+            bg='#FDFFAB'
+            border='1px solid #747474'
+            height='7px'
+            mr='2px'
+            mt='2px'
             display={activePage === 'A' ? 'block' : 'none'}
           />
         </Link>
