24b9d164b7bb0af28f96ac8f847e4c8c6a9bbb32
Marika <ferrari.marika@gmail.com>
2023-12-19 16:38:16 +0000

    Topbar almost completed

--- app/components/Topbar.js
+++ app/components/Topbar.js
@@ -1,19 +1,25 @@
-import { Avatar, AvatarBadge, Box, Container, Flex, Icon, Image, VStack } from '@chakra-ui/react';
+'use client';
+
+import { Avatar, AvatarBadge, Box, Container, Flex, Image, VStack } from '@chakra-ui/react';
+import Link from 'next/link';
+
+import { GiExitDoor } from 'react-icons/gi';
 
 export default function Topbar() {
   return (
-    <Container bg="#DDD9D1" position="fixed" top="0" minWidth="100%" minHeight="10%">
+    // Topbar sopra welcome
+    <Container bg="" position="fixed" top="0" minWidth="100%" minHeight="10%">
       <Flex justifyContent="space-between" alignItems="center">
         <VStack spacing={2}>
           <Box>
-            <Image src="RaadingRoomLogo.png" alt="Logo" height="50px" />
+            <Image src="RaadingRoomLogo.png" alt="Logo" boxSize="90px" />
           </Box>
         </VStack>
         <Box>
-          <Avatar src="MrTickle.png">
-            <AvatarBadge>
-              <Icon></Icon>
-            </AvatarBadge>
+          <Avatar src="Miffy.png" border="3px solid #747474" boxSize="65px" bg="white">
+            <Link href="/">
+              <AvatarBadge as={GiExitDoor} boxSize="40px" bg="#747474" color="red"></AvatarBadge>
+            </Link>
           </Avatar>
         </Box>
       </Flex>
