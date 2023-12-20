11c3d88f42a3ac869b7fca91ab71593b793e28a8
jacobfield <jacobprogrammes@gmail.com>
2023-12-20 16:44:06 +0000

    favicon.ico added but not working

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,22 @@
 // Global layout in common with all pages
 
-import { Providers } from './providers';
-import './globals.css';
+import { Providers } from "./providers";
+import "./globals.css";
 // Importing fonts object and passing it down as a prop
-import { fonts } from './fonts.js';
+import { fonts } from "./fonts.js";
 
 export const metadata = {
-  title: 'The Reading Room',
-  description: 'Created by The Blurb',
+  title: "The Reading Room",
+  description: "Created by The Blurb",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className={`${fonts.arvo.className} ${fonts.lato.className}`}>
+    <html
+      lang="en"
+      className={`${fonts.arvo.className} ${fonts.lato.className}`}
+    >
+      <link rel="icon" href="/favicon.ico" />
       <body>
         <Providers>{children}</Providers>
       </body>
